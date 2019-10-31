8493ea3d86752137470fcde0539b255f2172b02d
Cookedchicken <34245870+Cookedchicken@users.noreply.github.com>
2019-10-31 14:10:31 -0500

    updated chapter 1

---
+++ PersonalInfo/PersonalInfo/PersonalInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DeAntwan Hanes");
+            Console.WriteLine("Deantwanhanes@gamail.com");
+            Console.WriteLine("563-639-3145");
+        }
+
+    }
+}
---
+++ WineGlass/WineGlass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a wine glass graphic
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("X                  X");
+            Console.WriteLine(" X                 X");
+            Console.WriteLine(" X    X X X  X     X");
+            Console.WriteLine(" XX    X X X  X     ");
+            Console.WriteLine(" X    X X          X");
+            Console.WriteLine(" X    XXXX         X");
+
+
+
+
+        }
+    }
+}
